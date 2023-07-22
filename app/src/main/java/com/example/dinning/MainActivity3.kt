b85c08f5aaf30e9a38d6842a804967a2568a2d5d
ihayeong <hama01040@naver.com>
2023-07-22 17:13:30 +0900

    added comment for MainActivity.kt

--- app/src/main/java/com/example/dinning/MainActivity3.kt
+++ app/src/main/java/com/example/dinning/MainActivity3.kt
@@ -1,11 +1,29 @@
 package com.example.dinning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val buttonStart: Button = findViewById(R.id.btn_next)
+        val etName: AppCompatEditText = findViewById(R.id.et_id)
+
+        // TODO (STEP 3: Now validate name is entered or not and launch the QuizQuestion Activity.)
+        buttonStart.setOnClickListener {
+            if (etName.text.toString().isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this,MainActivity4::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
